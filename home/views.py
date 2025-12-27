6744173957d0e51c41ed91b6f1d87651530a8a52
Daniel Maia <daniel.maia.sousa@gmail.com>
2025-12-27 11:53:42 -0300

    cad_web7

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db import connection, DatabaseError
 from .models import *
+from .forms import *
 
 
 def index(request):
@@ -17,5 +18,12 @@ def categoria(request):
         'lista': Categoria.objects.all().order_by('-id'),
     }
     return render(request, 'categoria/lista.html',contexto)
+
+def form_categoria(request):
+    form = CategoriaForm()
+    contexto = {
+        'form': form,
+    }
+    return render(request, 'categoria/formulario.html',contexto)
 
 

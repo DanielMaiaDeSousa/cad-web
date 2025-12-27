6acda2030554967625264c46a9861900980d02ff
DanielMaiaDeSousa <daniel.maia.sousa@gmail.com>
2025-12-27 19:11:41 -0300

    Add form handling for Categoria in views.py

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db import connection, DatabaseError
 from .models import *
 from .forms import *
-
+from .forms import CategoriaForm  # Adicione esta linha se não existir
 
 def index(request):
     try:
@@ -20,10 +20,14 @@ def categoria(request):
     return render(request, 'categoria/lista.html',contexto)
 
 def form_categoria(request):
-    form = CategoriaForm()
+    if request.method == 'POST':
+       form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
+       if form.is_valid():# faz a validação do formulário
+            form.save() # salva a instancia do modelo no banco de dados
+            return redirect('categoria') # redireciona para a listagem
+    else:# método é get, novo registro
+        form = CategoriaForm() # formulário vazio
     contexto = {
-        'form': form,
+        'form':form,
     }
-    return render(request, 'categoria/formulario.html',contexto)
-
-
+    return render(request, 'categoria/formulario.html', contexto)
